e7bd06832ce379d2f8c24b5d0ea2b888a3e841ce
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-06-04 14:10:44 +0530

    Added red shade for negative values in getShade

--- public/script.js
+++ public/script.js
@@ -4,3 +4,9 @@ function getShade(value) {
     if (value >= 0) {
     const green = Math.min(255, 100 + value * 1.5);
     return `rgb(0, ${green}, 0)`;
+      } else {
+    const red = Math.min(255, 100 + Math.abs(value) * 1.5);
+    return `rgb(${red}, 0, 0)`;
+  }
+}
+
