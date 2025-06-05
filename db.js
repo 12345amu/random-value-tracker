d1398b0eb9bde9ca7159974f5c61e6823f888563
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-06-05 18:09:51 +0530

    Defined schema for storing random values

--- db.js
+++ db.js
@@ -9,3 +9,8 @@ const db = mongoose.connection;
 
 db.on('error', (err) => console.error('MongoDB connection error:', err));
 db.once('open', () => console.log('Connected to MongoDB with Mongoose'));
+
+const valueSchema = new mongoose.Schema({
+  value: Number,
+  timestamp: Date,
+});
