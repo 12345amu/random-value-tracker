abfdeeafe399851022decaedcb5bec3716a63b3e
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-06-05 17:41:02 +0530

    Handle MongoDB connection success and error events

--- db.js
+++ db.js
@@ -5,3 +5,7 @@ mongoose.connect(url, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+const db = mongoose.connection;
+
+db.on('error', (err) => console.error('MongoDB connection error:', err));
+db.once('open', () => console.log('Connected to MongoDB with Mongoose'));
