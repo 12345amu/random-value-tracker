2b24971ec51c7f8a6c769a215ede521842790e03
Amulya Kakumanu <kakumanuamulya5@gmail.com>
2025-06-06 11:38:31 +0530

    Initialized value, previousValue, positiveStreak, and streakStartTime variables

--- server.js
+++ server.js
@@ -1,28 +1,16 @@
 const express = require('express');
 const path = require('path');
-const { insertValue , value } = require('./db');
+const { insertValue , insertStreak } = require('./db');
 const app = express();
 const PORT = 5000;
 
 app.use(express.static(path.join(__dirname, 'public'))); 
 
-let currentValue = 0;                          
-let previousValue = 0;                        
+let currentValue = 0;
+let previousValue = 0;
+let positiveStreak = 0;
+let streakStartTime = null;
 
-setInterval(() => {                           
-  previousValue = currentValue;               
-  currentValue = Math.floor(Math.random() * 201) - 100;  
-  const timestamp = new Date();              
-  console.log(`[${timestamp.toISOString()}] Value: ${currentValue}`); 
-}, 1000);
-
-app.get('/api/current-value', (req, res) => {           
-  res.json({                                            
-    value: currentValue,                                
-    trend: currentValue > previousValue ? 'up' :        
-           currentValue < previousValue ? 'down' : 'same' 
-  });                                                   
-});
 
 
 app.listen(PORT, () => {
